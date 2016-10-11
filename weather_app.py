f329636b0b23c8a1ce1b740a9e537bb0bff5a600
Collo <delanhype@gmail.com>
2016-10-11 16:23:54 +0300

    first commit for app that consumes api

---
+++ weather_app.py
@@ -0,0 +1,23 @@
+"""Weather app
+
+--help
+If you get an error , re-check the spelling of the city/town.
+Though some places are not incorporated into the api, for example Turkana
+All major towns in Kenya have their info in the api
+
+
+"""
+from docopt import docopt #import docopt lib
+city = str(raw_input('Enter City/Town Name(e.g Nairobi) :  ')) #gets raw data from user
+#city='nairobi'
+import urllib2 #imports urllib2 library (am using python 2 thats why)
+response = urllib2.urlopen('http://api.wunderground.com/api/edac91112bb37041/conditions/q/CA/'+city+'.json') #equivalent to the HTTP verb of GET
+feedback = response.read() #info is read and stored in variable called feedback
+print feedback #info is printed for the user
+
+
+
+
+if __name__ == '__main__': #initialises the docopt lib
+    arguments = docopt(__doc__, version='my weather app')
+    print(arguments)
